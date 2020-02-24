7f5cac3aa52c2ced642245e8414c3611e1bce6c3
mrchou <imchou239@gmail.com>
2020-02-24 15:35:59 +0800

    Canny threshold can be set on construction

--- Operations/edges.py
+++ Operations/edges.py
@@ -8,7 +8,14 @@ from IMGBOX.Operations.base import SingularOperation
 class Canny(SingularOperation):
     """Simple Canny operation"""
 
+    def __init__(self, threshold1: float = 50.0, threshold2: float =200.0):
+        self._thres1 = threshold1
+        self._thres2 = threshold2
+
     def _operate(self, img: np.array) -> np.array:
-        result = cv2.Canny(img.astype(np.uint8, copy=False), 50, 200)
+        result = cv2.Canny(
+            img.astype(np.uint8, copy=False),
+            self._thres1, self._thres2
+        )
         result = cv2.cvtColor(result, cv2.COLOR_GRAY2BGR)
         return result
