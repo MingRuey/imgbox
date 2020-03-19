8793c0ea6e49db237bd9ca9ad1645a3dce09cbf4
mrchou <imchou239@gmail.com>
2020-03-19 10:32:07 +0800

    add laplacian edge detector; fix canny syntext; add 2d-cross-correlation

---
+++ Operations/correlation.py
@@ -0,0 +1,24 @@
+import numpy as np
+from scipy.signal import fftconvolve as fftconv
+
+from IMGBOX.core import Image
+from IMGBOX.Operations.base import BinaryOperation
+
+
+class CrossCorrelate2D(BinaryOperation):
+    """Calculate cross correlation between two images
+
+    It will first convert image to gray-scale, then conduct fftconvolve
+    """
+
+    def _operate(self, array1: np.array, array2: np.array) -> np.array:
+        img1 = np.sum(array1, axis=2)
+        img1 -= np.mean(img1)
+
+        img2 = np.sum(array2, axis=2)
+        img2 -= np.mean(img2)
+
+        result = fftconv(img1, img2[::-1, ::-1], mode="same")
+        result = (result - np.min(result))
+        result = (result / np.max(result)) * 255
+        return result
--- Operations/edges.py
+++ Operations/edges.py
@@ -8,7 +8,12 @@ from IMGBOX.Operations.base import SingularOperation
 class Canny(SingularOperation):
     """Simple Canny operation"""
 
-    def __init__(self, threshold1: float = 50.0, threshold2: float =200.0):
+    def __init__(self, threshold1: float = 50.0, threshold2: float = 200.0):
+        """
+        Args:
+            threshold1 (float): Low threshold for Canny. Defaults to 50.0.
+            threshold2 (float): High threshold for Canny. Defaults to 200.0.
+        """
         self._thres1 = threshold1
         self._thres2 = threshold2
 
@@ -16,6 +21,26 @@ class Canny(SingularOperation):
         result = cv2.Canny(
             img.astype(np.uint8, copy=False),
             self._thres1, self._thres2
+        )
+        result = cv2.cvtColor(result, cv2.COLOR_GRAY2BGR)
+        return result
+
+
+class Laplacian(SingularOperation):
+    """Simple Laplacian edge detector"""
+
+    def __init__(self, kernel_size: int = 3):
+        """
+        Args:
+            kernel_size (int): The size of Laplace kernel. Defaults to 3.
+        """
+        self._kern = kernel_size
+
+    def _operate(self, img: np.array) -> np.array:
+        img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+        result = cv2.Laplacian(
+            img.astype(np.uint8, copy=False),
+            ddepth=cv2.CV_8U, ksize=self._kern
         )
         result = cv2.cvtColor(result, cv2.COLOR_GRAY2BGR)
         return result
